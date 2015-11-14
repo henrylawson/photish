da17a716ce74eb8dc571b532246d3175eeeb8723
Henry Lawson <henry.lawson@foinq.com>
2015-11-14 15:09:54 +0800

    adding config file

--- lib/photish/config.rb
+++ lib/photish/config.rb
@@ -4,9 +4,9 @@ require 'active_support/core_ext'
 
 module Photish
   class Config
-    def initialize(config_path, override_hash)
-      config_hash = YAML.load_file(config_path).deep_symbolize_keys
-      @config_hash = config_hash.merge(override_hash.deep_symbolize_keys)
+    def initialize(config_file_path, override_hash)
+      config_file_hash = YAML.load_file(config_file_path).deep_symbolize_keys
+      @config_hash = config_file_hash.merge(override_hash.deep_symbolize_keys)
     end
 
     def val(key)
