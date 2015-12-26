e6037c11a5404fcd3250a3c750593d0ffd6b29ac
Henry Lawson <henry.lawson@foinq.com>
2015-12-26 09:23:43 +0800

    comma

--- lib/photish/log/safe_block.rb
+++ lib/photish/log/safe_block.rb
@@ -4,7 +4,7 @@ module Photish
       def handle_errors(name)
         begin
           yield
-        rescue ScriptError, SignalException, StandardError SystemExit => e
+        rescue ScriptError, SignalException, StandardError, SystemExit => e
           log.fatal "#{name} died due to #{e.class.name}"
           log.fatal e unless e.kind_of?(SystemExit)
           Thread.list.each(&:kill)
