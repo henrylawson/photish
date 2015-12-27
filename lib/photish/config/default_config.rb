41609cd00ac751096255a980435d2f775d88d663
Henry Lawson <henry.lawson@foinq.com>
2015-12-27 17:54:09 +0800

    use hard coded list, command is not multi platform

--- lib/photish/config/default_config.rb
+++ lib/photish/config/default_config.rb
@@ -24,14 +24,7 @@ module Photish
       private
 
       def image_extensions
-        out, _err, _status = Open3.capture3('convert -list format')
-        out.encode('UTF-8', 'UTF-8', invalid: :replace,
-                                     undef: :replace,
-                                     replace: '')
-           .split($/)
-           .map { |line| /(\S+)(?=\*)/.match(line).try(:[], 0) }
-           .compact
-           .map { |ext| ext.downcase }
+        ImageExtension::IMAGE_MAGICK
       end
 
       def url
---
+++ lib/photish/config/image_extension.rb
@@ -0,0 +1,8 @@
+module Photish
+  module Config
+    module ImageExtension
+      # Taken from 'convert -list format'
+      IMAGE_MAGICK = ['aai', 'art', 'avs', 'bgr', 'bgra', 'bgro', 'bmp', 'bmp2', 'bmp3', 'brf', 'cal', 'cals', 'canvas', 'caption', 'cin', 'cip', 'clip', 'cmyk', 'cmyka', 'cur', 'cut', 'data', 'dcm', 'dcx', 'dds', 'dfont', 'dpx', 'dxt1', 'dxt5', 'eps2', 'eps3', 'fax', 'fits', 'fractal', 'fts', 'g3', 'gif', 'gif87', 'gradient', 'gray', 'group4', 'h', 'hald', 'hdr', 'histogram', 'hrz', 'htm', 'html', 'icb', 'ico', 'icon', 'inline', 'ipl', 'isobrl', 'isobrl6', 'jng', 'jnx', 'jpe', 'jpeg', 'jpg', 'jps', 'label', 'mac', 'magick', 'map', 'mask', 'matte', 'miff', 'mng', 'mono', 'mpc', 'msl', 'mtv', 'mvg', 'null', 'otb', 'otf', 'pal', 'palm', 'pam', 'pango', 'pattern', 'pbm', 'pcd', 'pcds', 'pct', 'pcx', 'pdb', 'pes', 'pfa', 'pfb', 'pfm', 'pgm', 'picon', 'pict', 'pix', 'pjpeg', 'plasma', 'png', 'png00', 'png24', 'png32', 'png48', 'png64', 'png8', 'pnm', 'ppm', 'preview', 'ps2', 'ps3', 'psb', 'psd', 'ptif', 'pwp', 'radial-gradient', 'ras', 'rgb', 'rgba', 'rgbo', 'rgf', 'rla', 'rle', 'scr', 'sct', 'sfw', 'sgi', 'shtml', 'six', 'sixel', 'sparse-color', 'stegano', 'sun', 'text', 'tga', 'thumbnail', 'tiff', 'tiff64', 'tile', 'tim', 'ttc', 'ttf', 'txt', 'ubrl', 'ubrl6', 'uil', 'uyvy', 'vda', 'vicar', 'vid', 'viff', 'vips', 'vst', 'wbmp', 'wpg', 'xbm', 'xc', 'xcf', 'xpm', 'xv', 'ycbcr', 'ycbcra', 'yuv']
+    end
+  end
+end
