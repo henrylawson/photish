5f0a9706c99f8044503a3d85a76158b8791ee370
Henry Lawson <henry.lawson@foinq.com>
2016-01-17 14:28:54 +0800

    ensuring it is all read only

--- packaging/after-install.sh
+++ packaging/after-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 ln -fs /usr/local/lib/PACKAGE_PLACEHOLDER/photish /usr/local/bin/photish
+chmod -R a+r /usr/local/lib/PACKAGE_PLACEHOLDER
 chmod a+rw /usr/local/lib/PACKAGE_PLACEHOLDER/lib/vendor/Gemfile.lock
