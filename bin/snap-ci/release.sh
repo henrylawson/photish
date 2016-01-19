dc384bbc6bd6e0929d09a2d013dc8d67971798c7
Henry Lawson <henry.lawson@foinq.com>
2016-01-19 20:11:51 +0800

    save time

--- bin/snap-ci/release.sh
+++ bin/snap-ci/release.sh
@@ -5,18 +5,20 @@ set -xeuo pipefail
 export COMMIT=$SNAP_COMMIT
 export BRANCH=$SNAP_BRANCH
 
-# determine script dir
-DIR="$(dirname "$(readlink -f "$0")")"
-
-# setup dependencies and ruby
-source "$DIR/setup.sh"
-
-# ls the artifact dir
-ls -larth ./pkg
 
 # conditionally release app
 if git describe --exact-match $COMMIT && [ $BRANCH = 'master' ]
 then
+  # determine script dir
+  DIR="$(dirname "$(readlink -f "$0")")"
+
+  # setup dependencies and ruby
+  source "$DIR/setup.sh"
+
+  # ls the artifact dir
+  ls -larth ./pkg
+
+  # perform the release
   bundle exec rake release
 else
   echo "This is not a tagged commit on master, skipping release."
