663a594d52b23d2a1420f14801c8b228a57871c0
Henry Lawson <henry.lawson@foinq.com>
2016-01-19 18:45:56 +0800

    map here

--- bin/snap-ci/pack.sh
+++ bin/snap-ci/pack.sh
@@ -4,6 +4,7 @@ set -xeuo pipefail
 # map values from snap-ci
 export COMMIT=$SNAP_COMMIT
 export BRANCH=$SNAP_BRANCH
+export TEMP_DIR=$SNAP_CACHE_DIR
 
 # determine script dir
 DIR="$(dirname "$(readlink -f "$0")")"
