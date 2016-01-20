8a7aabe426ff39cbbbfbf7bf75d522632f2edc97
Henry Lawson <henry.lawson@foinq.com>
2016-01-20 22:13:28 +0800

    rpm packages tested and working on rpm

--- boxes/centos_i386/test-install.sh
+++ boxes/centos_i386/test-install.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 set -xeuo pipefail
 
+# install local package
 cp /photish/pkg/*i386.rpm ~
 sudo rpm -e photish || true
 sudo rpm -Uh ~/*i386.rpm
+photish version
+
+# uninstall
+sudo rpm -e photish
+
+# download repo file
+wget https://bintray.com/henrylawson/rpm/rpm -O bintray-henrylawson-rpm.repo
+sudo mv bintray-henrylawson-rpm.repo /etc/yum.repos.d/
+sudo yum install -y photish
 photish version
