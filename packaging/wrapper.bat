18a4c6ba5c7669034b7a8247ef71727a104a1b16
Dave Leaver <danzel@localhost.geek.nz>
2016-07-03 13:28:24 +1200

    Fix running on windows

--- packaging/wrapper.bat
+++ packaging/wrapper.bat
@@ -5,4 +5,4 @@ set "BUNDLE_GEMFILE=%~dp0\lib\vendor\Gemfile"
 set BUNDLE_IGNORE_CONFIG=
 
 :: Run the actual app using the bundled Ruby interpreter, with Bundler activated.
-@"%~dp0\lib\ruby\bin\ruby.bat" -rbundler/setup "%~dp0\lib\app\hello.rb"
+@"%~dp0\lib\ruby\bin\ruby.bat" -rbundler/setup "%~dp0\lib\app\exe\photish" %*
